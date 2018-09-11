a7f2b5c85cd822f3d5157608a6d14cd640b91dde
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-09-11 15:28:40 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180908035520) do
+ActiveRecord::Schema.define(version: 20180911061747) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180908035520) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
