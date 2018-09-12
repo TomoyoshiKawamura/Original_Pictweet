05250fc35d494f9a1a3e8c85c19b986412a13cd1
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-09-12 17:51:13 +0900

    TweetsCommentController

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    Comment.create(text:params[:text],tweet_id:params[:tweet_id],user_id:current_user.id)
+    redirect_to "/tweets/#{@comment.tweet.id}"
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  private
+
+  def comment_params
+    params.permit(:text,:tweet_id)
+  end
+
+end
